f0e348261320f00311afed5ba3dfd0909305fc4d
Miguel Hortelano <92085384+Qaraqororum@users.noreply.github.com>
2023-08-14 17:17:59 +0200

    Update extra_architectures.py
    adaptacion de canales y clases según entrenamiento

--- ml4floods/models/architectures/extra_architectures.py
+++ ml4floods/models/architectures/extra_architectures.py
@@ -12,8 +12,8 @@ class MAtentionNet(nn.Module):
         self.conv  = smp.MAnet(
             encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
             encoder_weights="imagenet"
-            in_channels=13,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-            classes=3,                      # model output channels (number of classes in your dataset)
+            in_channels=n_channels,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=n_class,                      # model output channels (number of classes in your dataset)
             )
       
 
