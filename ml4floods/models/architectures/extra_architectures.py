e13fe4b3705910f2696ec2697a7317b9897250b5
Miguel Hortelano <92085384+Qaraqororum@users.noreply.github.com>
2023-08-14 17:11:39 +0200

    Update extra_architectures.py

--- ml4floods/models/architectures/extra_architectures.py
+++ ml4floods/models/architectures/extra_architectures.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import segmentation_models_pytorch as smp
+from segmentation_models_pytorch.encoders import get_preprocessing_fn
 
+preprocess_input = get_preprocessing_fn('resnet18', pretrained='imagenet')
 
 class MAtentionNet(nn.Module):
 
@@ -9,6 +11,7 @@ class MAtentionNet(nn.Module):
 
         self.conv  = smp.MAnet(
             encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights="imagenet"
             in_channels=13,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=3,                      # model output channels (number of classes in your dataset)
             )
